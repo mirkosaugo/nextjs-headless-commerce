94ac6d86c2a4d6183d4714266647d0097cda0e73
Mirko Saugo <himynameismirko@gmail.com>
2022-06-13 01:50:16 +0200

    Navbar component test

---
+++ components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Navbar.module.scss'
+
+const Navbar = () => {
+  return (
+    <div className={style.wrapper}>Navbar</div>
+  )
+}
+
+export default Navbar
