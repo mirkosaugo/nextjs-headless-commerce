4933fe1701c4c28f64aed666d47e6a39734b768e
Mirko Saugo <himynameismirko@gmail.com>
2022-06-13 01:49:59 +0200

    nextconfig added global sass

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
+const path = require('path')
+
+
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    additionalData: '@import "assets/scss/config.scss";'
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return config
+  },
 }
 
 module.exports = nextConfig
