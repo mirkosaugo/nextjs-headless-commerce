fad4744d904760e58a05b5bc3c3914299cda1a87
Mirko Saugo <himynameismirko@gmail.com>
2022-06-14 00:29:08 +0200

    Navbar module

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
+// components
+import Container from '../Container'
+
 import style from './Navbar.module.scss'
 
 const Navbar = () => {
   return (
-    <div className={style.wrapper}>Navbar</div>
+    <nav className={style.wrapper}>
+      <Container>
+        <Link href="/">
+          <a className={style.logo}>E-Sanity</a>
+        </Link>
+        <button onClick={() => alert('dio porco')} className={style.menu}>Menu</button>
+        <button className={style.cart}>Cart</button>
+      </Container>
+    </nav>
   )
 }
 
